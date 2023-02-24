3830dbe81f752bb70aaf9778ca5479a8fd805fbf
VVsxmja <vvsxmja@qq.com>
2023-02-25 03:35:46 +0800

    add check for warning

--- Makefile
+++ Makefile
@@ -3,8 +3,13 @@ all: data-lab
 mkdir: 
 	mkdir -p results
 
-data-lab: clean mkdir
-	cd data-lab && make clean && make && ./btest > ../results/data-lab.txt
+data-lab: data-lab-check data-lab-test
+
+data-lab-check: clean mkdir
+	cd data-lab && chmod +x ./dlc && ./dlc bits.c > ../results/data-lab-check.txt
+
+data-lab-test: clean mkdir
+	cd data-lab && make clean && make && ./btest > ../results/data-lab-test.txt
 
 clean:
 	rm -rf results
--- readme-generator/main.js
+++ readme-generator/main.js
@@ -1,14 +1,26 @@
+import * as fs from 'fs/promises'
+import * as path from 'path'
+import { Octokit } from 'octokit'
+
 /* parse result */
 
 const resultPath = '../results'
 
 class Score {
-    constructor(score, total) {
+    constructor(score, total, other) {
         this.score = score
         this.total = total
+        this.other = other
+        this.other.toString = function () {
+            let r = ''
+            if (this.warning > 0) {
+                r += `⚠️ ${this.warning} warning(s)`
+            }
+            return r
+        }
     }
     toString() {
-        return `${this.score}/${this.total}`
+        return `${this.score} / ${this.total}, ${this.other}`
     }
     get done() {
         return this.score === this.total
@@ -18,10 +30,17 @@ class Score {
 const resultObjects = [
     {
         name: 'Data Lab',
-        resultFileName: 'data-lab.txt',
-        parse(content) {
+        async autoCheck() {
+            const content = await fs.readFile(path.join(resultPath, 'data-lab-test.txt'), {
+                encoding: 'utf-8'
+            })
             const result = /Total points: (?<score>\d+)\/(?<total>\d+)/.exec(content)
-            return new Score(parseInt(result.groups.score), parseInt(result.groups.total))
+            const checkContent = await fs.readFile(path.join(resultPath, 'data-lab-check.txt'), {
+                encoding: 'utf-8'
+            })
+            this.score = new Score(parseInt(result.groups.score), parseInt(result.groups.total), {
+                warning: checkContent.match(/dlc:bits.c:\d+:.*: Warning:/g).length
+            })
         }
     },
     { name: 'Bomb Lab' },
@@ -29,15 +48,12 @@ const resultObjects = [
     { name: 'Buffer Lab (IA32)' },
     { name: 'Architecture Lab' },
     { name: 'Architecture Lab (Y86)' },
-    { name: 'Cache La' },
+    { name: 'Cache Lab' },
     { name: 'Performance Lab' },
     { name: 'Shell Lab' },
     { name: 'Malloc Lab' },
     { name: 'Proxy Lab' },
 ]
-
-import * as fs from 'fs/promises'
-import * as path from 'path'
 
 if (!fs.access('../results')) {
     console.error('Result directory not exist')
@@ -45,23 +61,15 @@ if (!fs.access('../results')) {
 }
 
 for (const target of resultObjects) {
-    if (!target.parse) {
+    if (!target.autoCheck) {
         console.log(`Auto check of ${target.name} not implemented yet`)
-        continue
-    }
-    if (!target.resultFileName) {
-        console.warn(`Result file name of ${target.name} not specified`)
-    }
-    try {
-        const content = await fs.readFile(path.join(resultPath, target.resultFileName), {
-            encoding: 'utf-8'
-        })
-        target.score = target.parse(content)
-    } catch (e) {
-        if (e?.code === 'ENOENT') {
-            console.warn(`Result of ${target.name} not exist`)
+    } else {
+        try {
+            await target.autoCheck()
+            console.error(target.score)
+        } catch (e) {
+            console.error(`Failed parsing result of ${target.name}: \n`, e)
         }
-        console.error(`Failed parsing result of ${target.name}: \n`, e)
     }
 }
 
@@ -69,9 +77,10 @@ let logParsed = 'Parsed result:\n'
 let finalMarkdown = '|   |Lab|Score|\n|---|---|---|\n'
 
 for (const target of resultObjects) {
+    console.log(target)
     if (!!target.score) {
         logParsed += `\t${target.name}:\t ${target.score}\n`
-        finalMarkdown += `| ${target.score.done ? '💯' : '📝'} | ${target.name} | **\`${target.score.score}\`** / **\`${target.score.total}\`** | \n`
+        finalMarkdown += `| ${target.score.done ? '💯' : '📝'} | ${target.name} | **\`${target.score}\`** | \n`
     } else {
         logParsed += `\t${target.name}:\t Not parsed\n`
         finalMarkdown += `| ⏳ | ${target.name} | *\`Not implemented\`* | \n`
@@ -80,7 +89,6 @@ for (const target of resultObjects) {
 
 console.log(logParsed)
 
-import { Octokit } from "octokit"
 
 const octokit = new Octokit({ auth: process.env.TOKEN })
 
@@ -109,7 +117,7 @@ if (beginIndex === -1 || endIndex === -1) {
         + finalMarkdown + readme.substring(endIndex)
     await octokit.request(`PUT /repos/${repo}/contents/{path}`, {
         path: readmeData.path,
-        message: `Automatically update ${readmeData.name} by AutoGrade`,
+        message: `🚀 Automatically update ${readmeData.name} by AutoGrade`,
         content: Buffer.from(newReadme).toString('base64'),
         sha: readmeData.sha,
     })
